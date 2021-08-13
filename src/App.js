5887124d29f65a69702878e60a9062bf7a501dce
Monde Sineke <nsineke@gmail.com>
2021-08-13 14:33:55 +0200

    feat/css-reset (#8)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
+import { Reset } from "styled-reset";
 
+import GlobalStyle from "./globalStyles";
 import ConfirmedRegionList from "./features/confirmedRegions/List";
 
 function App() {
   return (
     <div className="App">
+      <Reset />
+      <GlobalStyle />
       <ConfirmedRegionList />
     </div>
   );
